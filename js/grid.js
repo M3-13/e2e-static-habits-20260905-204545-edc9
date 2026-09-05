f4f3e10e337672e2b75ef560157c444d7ccebb60
office-crew <development@office-crew.dev>
2026-09-05 21:01:24 +0200

    feat: Grundgerüst mit Store und Persistenz aufsetzen
    Closes #7

---
+++ js/grid.js
@@ -0,0 +1,1 @@
+export function renderGrid(container, habit) {}
---
+++ js/stats.js
@@ -0,0 +1,11 @@
+export function currentStreak(habit) {
+  return 0;
+}
+
+export function longestStreak(habit) {
+  return 0;
+}
+
+export function weekCompletion(habit) {
+  return 0;
+}
